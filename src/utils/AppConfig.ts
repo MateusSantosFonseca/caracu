cf3a99279d233236124e63d24f76c223fef714e4
Mateus Fonseca <45724993+MateusSantosFonseca@users.noreply.github.com>
2023-11-19 23:54:18 -0300

    Initial commit

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+export const AppConfig = {
+  site_name: 'Caracu',
+  title: 'Caracu',
+  description:
+    'Caracu vai te ajudar a sortear os times de formas equilibradas!',
+  locale: 'pt=br',
+};
