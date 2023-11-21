3d84c6e51a4b70266f9d9025d1a617ceb8a09d45
Mateus Fonseca <45724993+MateusSantosFonseca@users.noreply.github.com>
2023-11-21 00:21:45 -0300

    fix: fixing production URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://main--charming-tartufo-c50854.netlify.app/', // FIXME: Change to the production URL
+  siteUrl: 'https://scintillating-vacherin-9841c3.netlify.app/', // FIXME: Change to the production URL
   generateRobotsTxt: true,
 };
