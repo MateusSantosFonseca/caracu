88af78a8c78ec84ed0be26480c5fd12df727e8ca
Mateus Fonseca <45724993+MateusSantosFonseca@users.noreply.github.com>
2023-11-22 04:06:33 -0300

    refactor(component): removing comments

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://scintillating-vacherin-9841c3.netlify.app/', // FIXME: Change to the production URL
+  siteUrl: 'https://scintillating-vacherin-9841c3.netlify.app/',
   generateRobotsTxt: true,
 };
