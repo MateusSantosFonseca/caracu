6be534a3cfd0f3b459530d721bbb8e3437dcd99d
Mateus Fonseca <45724993+MateusSantosFonseca@users.noreply.github.com>
2023-11-23 00:37:08 -0300

    fix(structure): fix pwa

--- next.config.mjs
+++ next.config.mjs
@@ -28,12 +28,11 @@ const nextConfig = bundleAnalyzer({
 });
 
 export default withPWA({
-  reactStrictMode: true,
   pwa: {
     dest: 'public',
     register: true,
     skipWaiting: true,
-    buildExcludes: [/middleware-manifest.json$/],
+    buildExcludes: [/middleware-manifest.json$/, /sw.js$/],
   },
   disable: process.env.NODE_ENV === 'development',
 })(nextConfig);
